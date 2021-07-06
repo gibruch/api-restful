b53a097cf9a3041bb53d075c81da6ca554c108e2
Gilberto Bruch <gilberto.bruch@gmail.com>
2021-07-06 15:52:06 -0300

    adicionando feature model e controler de produtos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,20 @@
 const router = require('express').Router()
 
-// definindo rota : clientes
-router.get('/clientes', (req, res) => {
-    res.send( {
-        ok: 123
-    })
-})
+const ProductController = require('../controllers/products')
 
+/**
+ * VERBOS HTTP (4 tipos basicos)
+ * 
+ *  GET = obter dados
+ *  POST = enviar(cliente) / receber(servidor) dados
+ *  PUT = atualizar dados
+ *  DELETE = remover dados
+ * 
+ */
+
+// definindo rota : products
+router.get('/products', ProductController.get)
+// router.post('/products', ProductController.post)
+// router.put('/products/:id', ProductController.put)
+// router.delete('/products/:id', ProductController.delete)
 module.exports = router
